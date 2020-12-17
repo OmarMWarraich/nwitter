d16d3619c74067fa7bc25007718d4a3e212a3d36
Kirito <kirito61254@gmail.com>
2020-12-17 12:52:43 +0000

    5.2 Update Profile Bugfix

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,7 +2,7 @@ import { authService, dbService } from "fbase";
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = ({ userObj }) => {
+const Profile = ({ refreshUser, userObj }) => {
   const history = useHistory();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
@@ -27,6 +27,7 @@ const Profile = ({ userObj }) => {
       await userObj.updateProfile({
         displayName: newDisplayName,
       });
+      refreshUser();
     }
   };
   return (
